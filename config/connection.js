9fceca23d9d0dc2ef8eeb03a47515cee36c629c4
Chris Crawford <chrisccrawford@utexas.edu>
2016-07-30 14:46:17 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql           = require('mysql');
 var connection      = mysql.createConnection({
     port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
+    host: 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'weq3ouh7rhigyyp0',
+    password: 'btysjfq9465l3y5g',
+    database: 'keg43p251cr82uro'
 });
 
 connection.connect(function(err) {
